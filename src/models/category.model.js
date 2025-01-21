43d16595828b593a28c34e21a1b45397f4829faa
suphal-dev <suphal@klizos.com>
2025-01-21 12:43:33 +0530

    category model added

---
+++ src/models/category.model.js
@@ -0,0 +1,26 @@
+import mongoose ,{Schema} from "mongoose";
+
+
+
+
+const categorySchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+  
+  
+    
+
+},{timestamps:true})
+
+
+
+
+
+
+
+
+
+
+export const Category=mongoose.model("Category",categorySchema)
