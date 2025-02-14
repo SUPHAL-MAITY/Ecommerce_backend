5975cc3c6110e3b32108279ca9288a80c9d66e84
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-14 22:46:48 +0530

    order item routes

--- src/routes/orderItems.routes.js
+++ src/routes/orderItems.routes.js
@@ -1,11 +1,11 @@
 import {Router } from "express";
-import { createOrderItemsControler } from "../controllers/orderItems.controller.js";
+import { createOrderItemsControler, getRecentOrdersController } from "../controllers/orderItems.controller.js";
 
 
 const router=Router();
 
 
-
+router.route("/get-recent-orders").get(getRecentOrdersController)
 router.route("/create-order-items").post(createOrderItemsControler)
 
 export default router;
