597616a31287fad494b87ba1458665e047dfd9f1
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-14 22:46:09 +0530

    product routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,23 +1,23 @@
 import { Router } from "express";
-import {createProductController, deleteProductController, filteredProductsController, getAllProductsController, getAllProductsWithReviewController, searchedProductsController, singleProductController, updateProductController } from "../controllers/products.controller.js";
+import {createProductController, deleteProductController, filteredProductsController, getAllProductsAdmincontroller, getAllProductsController, getAllProductsWithReviewController, searchedProductsController, singleProductController, updateProductController } from "../controllers/products.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwtToken,isAdmin } from "../middlewares/auth.middleware.js";
 
 
 
 
-//just a comment
+
 
 
 const router=Router()
 
-
+router.route("/get-all-products").get( getAllProductsController)
+router.route("/get-all-admin-products").get( getAllProductsAdmincontroller)
+router.route("/get-single-product/:productId").get(singleProductController)
 router.route("/get-all-products-with-review").get(getAllProductsWithReviewController)
 router.route("/filter").get(filteredProductsController)
 router.route("/search").get(searchedProductsController)
 router.route("/create-product").post( upload.array("images",4) ,createProductController)
-router.route("/get-all-products").get( getAllProductsController)
-router.route("/get-single-product/:productId").get(singleProductController)
 router.route("/update/:productId").put(verifyJwtToken,isAdmin,updateProductController)
 router.route("/delete").delete(verifyJwtToken,isAdmin,deleteProductController)
 
