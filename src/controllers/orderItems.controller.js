41fbc229d626e9c27e82ddb7a9fa4debdc8a5d63
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-15 19:49:57 +0530

    order item controller for recent order

--- src/controllers/orderItems.controller.js
+++ src/controllers/orderItems.controller.js
@@ -37,10 +37,22 @@ export const getRecentOrdersController=asyncHandler(async(req,res)=>{
     const limit=req.query.limit||5;
     const skip=(page -1)*limit;
 
-    const orders=await OrderItems.find({}).skip(skip).limit(limit).populate("Orders").sort({createdAt:-1})
+    const orders=await OrderItems.find({}).skip(skip).limit(limit).populate("productId").populate("orderId").sort({createdAt:-1})
 
     if(!orders){
         throw new ApiError(400,"No orders found ")
+    }
+
+
+    const totalOrders=await OrderItems.countDocuments()
+    if(!totalOrders){
+        throw new ApiError(400,"total orders not found")
+    }
+
+
+    const totalPages=Math.ceil(totalOrders/limit)
+    if(!totalPages){
+        throw new ApiError(400,"pages not found while counting the total pages")
     }
 
     return res.status(200).json(new ApiResponse(200,{orders,totalPages},"users fetched successfully"))
